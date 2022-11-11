41fb2d7004d53a1da556a4beb4f7199d9b8ad790
Regi Apriandi <69528812+regiapriandi012@users.noreply.github.com>
2022-11-11 10:06:29 +0700

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-def get_db_handle(db_name, host, port, username, password):
-
- client = pymongo.MongoClient('mongodb+srv://regiapriandi012:Sinheul24.@instance2.jrd6j.mongodb.net/?retryWrites=true&w=majority')
- db_handle = client['registrationdatabase']
- return db_handle, client
